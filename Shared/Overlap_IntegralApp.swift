619a8c16197ef9c45bff4cd42c5ff9e102fca961
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-20 15:38:48 -0600

    Initial Commit

---
+++ Shared/Overlap_IntegralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_IntegralApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_IntegralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
